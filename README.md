1776534cfe2b3bf181a671e4b12a2e642f06fdc5
Alex Milanov <alex@i4web.biz>
2017-06-02 17:46:30 +0300

    initial integration prototype

---
+++ README.md
@@ -0,0 +1,7 @@
+# Node Python Integration Prototype
+- `npm i` to install dependencies
+- then run via `node index.js` or just `nodemon`
+
+## REF:
+- https://github.com/extrabacon/python-shell
+- http://ourcodeworld.com/articles/read/286/how-to-execute-a-python-script-and-retrieve-output-data-and-errors-in-node-js
---
+++ index.js
@@ -0,0 +1,17 @@
+var PythonShell = require('python-shell');
+var pyshell = new PythonShell('script.py');
+
+pyshell.send(JSON.stringify([1,2,3,4,5]));
+
+pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    console.log(message);
+});
+
+// end the input stream and allow the process to exit
+pyshell.end(function (err) {
+    if (err){
+        throw err;
+    };
+    console.log('finished');
+});
